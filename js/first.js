f746106d155c32fd5ab65b16d1b0034de7a0cc82
Ashitosh2727 <maneashitosh27@gmail.in>
2025-04-06 20:48:08 +0530

    Operators in js

---
+++ js/first.js
@@ -0,0 +1,67 @@
+console.log("Hello World");
+console.log("Ashitosh!");
+
+age = 27;
+console.log(age);
+
+name = "Ashitosh Mane";
+console.log(name);
+
+price = 277.27;
+console.log(price);
+
+x = null;
+console.log(x);
+
+y = undefined;
+console.log(y);
+
+isFollow = true;
+console.log(isFollow);
+
+let myName = "Ashitosh Mane";
+console.log(myName);
+
+const pi = 3.14;
+console.log(pi);
+
+let type = typeof isFollow;
+console.log(type);
+
+let big = BigInt("127");
+console.log(big);
+
+let sy = Symbol("Hello!");
+console.log(sy);
+
+const student = {
+    fullName : "Ashitosh Mane",
+    age : 21,
+    cgpa : 7.7,
+    isPass : true,
+};
+
+student ["age"] = student ["age"] + 1;
+
+console.log(student ["age"]);
+
+// practice question : 1
+const product = {
+    title : "Ball Pen",
+    ratings : 4,
+    offer : 5,
+    price : 270,
+};
+
+console.log(product);
+
+// practice question : 2
+const profile ={
+    name : "ShradhaKhapra",
+    isFollow : true,
+    posts : 195,
+    followers : 569 ,
+    following : 4,
+};
+
+console.log(profile);
---
+++ js/operators.js
@@ -0,0 +1,43 @@
+//This code prints hello world
+// console.log("Hello World");
+
+//Arithmetic operators
+// let a = 5;
+// let b = 2;
+
+// console.log("a + b =",a+b);
+// console.log("a - b =",a-b);
+// console.log("a * b =",a*b);
+// console.log("a / b =",a/b);
+// console.log("a % b =",a%b);
+// console.log("a ** b =",a**b);
+
+//Unary Operators
+// let a = 5;
+// let b = 2;
+
+// // a = a + 1;
+// // a++;
+// // a = a - 1;
+// a--;
+// console.log("a =",a);
+
+//Assignment Operators
+// let a = 5;
+// let b = 2;
+
+// a += 4; // a = a + 1
+// console.log("a =",a);
+
+//comparison Operators
+// let a = 5;
+// let b = 2;
+
+// console.log("a==b",a == b);
+// console.log("a < b",a < b);
+
+//Logical Operators
+let a = 6;
+let b = 5;
+
+console.log("cond1 && cond2 =",a > b && a === 6);
