faf5f1b28a1a8c93396e116a969228a7cc10e8e8
Ashitosh2727 <maneashitosh27@gmail.in>
2025-04-11 17:59:29 +0530

    strings in js

---
+++ js/strings.js
@@ -0,0 +1,40 @@
+//create string
+// let str = "Ashitosh";
+// let str1 = 'Apna College';
+
+// console.log(str[0]);
+
+//template literals
+// let sentence = `This is a template literal`;
+
+// console.log("Apna\n College");
+
+//String methods/functions
+// let str = "   Ashitosh";
+
+// console.log(str.trim());
+
+//Slice method
+
+//Concat Method
+// let str1 = "Ashitosh";
+// let str2 = "Mane";
+
+// console.log(str1.concat(str2));
+
+//Replace method
+// let str = "Hello";
+
+// console.log(str.replace("H","c"));
+
+// let str = "I Love Js";
+
+// console.log(str.charAt(0));
+
+
+//Practice set 1
+
+let fullName = prompt("Enter your full name");
+
+let userName =  "@" + fullName + fullName.length;
+console.log(userName);
